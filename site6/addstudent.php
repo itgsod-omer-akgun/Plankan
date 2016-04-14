a3f7255820ef7cb223db1b60e80af7d2f509c2b2
itgsod-omer-akgun <omer.akgun@elev.it-gymnasiet.se>
2016-04-14 13:04:24 +0200

    mysql och php

---
+++ site6/addstudent.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+<title>Add User</title>
+</head>
+<body>
+<form action="http://localhost/studentadded.php" method="post">
+
+<b>Add a New User</b>
+
+<p>First Name:
+<input type="text" name="first_name" size="30" value="" />
+</p>
+
+<p>Last Name:
+<input type="text" name="last_name" size="30" value="" />
+</p>
+
+<p>Email:
+<input type="text" name="email" size="60" value="" />
+</p>
+
+<p>Street:
+<input type="text" name="street" size="30" value="" />
+</p>
+
+<p>City:
+<input type="text" name="city" size="30" value="" />
+</p>
+
+<p>Zip Code:
+<input type="text" name="zip" size="30" maxlength="5" value="" />
+</p>
+
+<p>Phone Number:
+<input type="text" name="phone" size="30" value="" />
+</p>
+
+<p>Homephone Number*:
+<input type="text" name="phone" size="30" value="" />
+</p>
+
+<p>Birth Date (YYYY-MM-DD):
+<input type="text" name="birth_date" size="30" value="" />
+</p>
+
+</form>
+</body>
+</html>
